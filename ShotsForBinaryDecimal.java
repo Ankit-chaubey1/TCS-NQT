1cc356f8cd930b03309453d157df2537d291768d
9534495797 <chaubeyankit927@gmail.com>
2026-03-08 21:47:22 +0530

    method for binary to decimal or octa

---
+++ ShotsForBinaryDecimal.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class ShotsForBinaryDecimal {
+    public static void main(String[]args){
+        System.out.println(Integer.parseInt("1010",10));
+        System.out.println(Integer.parseInt("1010",2));
+    }
+}
